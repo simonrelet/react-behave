aca6cf75a2a9c30b0339d2cf2d2d4b5d17bea6b1
Simon Relet <simon.relet@adotmob.com>
2019-01-29 09:55:20 +0100

    chore: keep react-behave code

--- packages/react-libraries/config/babelTransform.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const babelJest = require('babel-jest')
-const createBabelConfig = require('./createBabelConfig')
-
-module.exports = babelJest.createTransformer(
-  createBabelConfig({ modules: 'commonjs' })
-)
